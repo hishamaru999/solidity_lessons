36bbe5b55a462b1629c104cadad6e0320fffcc8b
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2020-12-15 15:44:42 -0800

    Update mycontract.sol
    added a constant and used 'public' so I don't need get() anymore

--- mycontract.sol
+++ mycontract.sol
@@ -2,14 +2,15 @@
  pragma solidity >0.5.1 <0.8.0;
  
  contract MyContract {
-     string public value;
+     string public constant value = "hachama";
+     string public value2;
      
      constructor() public {
-         value = "hachamachamachama";
+         value2 = "chama";
      }
      
-     function set(string memory _value) public {
-         value = _value;
+     function set(string memory _value2) public {
+         value2 = _value2;
      }
      
  }
