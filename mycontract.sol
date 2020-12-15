41b36788fec4e7c085b23a1579ff5fcaf7cae20c
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2020-12-15 15:37:16 -0800

    Update mycontract.sol
    updated code to be compliant with latest version of solidity

--- mycontract.sol
+++ mycontract.sol
@@ -1,15 +1,11 @@
  //SPDX-License-Identifier: GPL-3.0
- pragma solidity >0.5.99 <0.8.0;
+ pragma solidity >0.5.1 <0.8.0;
  
  contract MyContract {
-     string value;
+     string public value;
      
      constructor() public {
          value = "hachamachamachama";
-     }
-     
-     function get() public view returns(string memory) {
-         return value;
      }
      
      function set(string memory _value) public {
