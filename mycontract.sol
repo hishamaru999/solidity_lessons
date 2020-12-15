57b1020ae578efda014530d7fce878b58c7ee333
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2020-12-15 15:57:47 -0800

    Update mycontract.sol
    added different variable types

--- mycontract.sol
+++ mycontract.sol
@@ -1,9 +1,18 @@
  //SPDX-License-Identifier: GPL-3.0
  pragma solidity >0.5.1 <0.8.0;
  
+ /** @title Hachama  */
  contract MyContract {
-     string public constant value = "hachama";
+     string public constant stringValue = "hachama";
      string public value2;
+     bool public myBool = true;
+     
+     // Interger type, can be signed (i.e. it can be negative)
+     int public myInt = -1;
+     
+     // Unsigned Integer type, cannot be negative
+     uint public myUint = 1;
+     
      
      constructor() public {
          value2 = "chama";
