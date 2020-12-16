21bab5e43caf9ea6f815bacfb2570fa7cb4f6d32
hishamaru999 <58763968+hishamaru999@users.noreply.github.com>
2020-12-15 16:15:36 -0800

    Update mycontract.sol
    added enumerated list and respective functions

--- mycontract.sol
+++ mycontract.sol
@@ -1,8 +1,36 @@
  //SPDX-License-Identifier: GPL-3.0
  pragma solidity >0.5.1 <0.8.0;
  
- /** @title Hachama  */
+ /** @title Hachama Status  */
  contract MyContract {
+     
+     // Enumerated list
+     enum State {Waiting, Ready, Active}
+     State public state;
+     
+     constructor() public {
+         state = State.Waiting;
+     }
+     
+     // Set status to Active
+     function activate() public {
+         state = State.Active;
+     }
+     
+     // Check to see if State is Active
+     function isActive() public view returns(bool) {
+         return state == State.Active;
+     }
+     
+     // Set status to Waiting
+     function deactivate() public {
+         state = State.Waiting;
+     }
+     
+     
+     
+     
+     // Various types of variables
      string public constant stringValue = "hachama";
      string public secondValue;
      bool public myBool = true;
@@ -13,10 +41,6 @@
      // Unsigned Integer type, cannot be negative
      uint public myUint = 1;
      
-     
-     constructor() public {
-         secondValue = "chama";
-     }
      
      function set(string memory _secondValue) public {
          secondValue = _secondValue;
